acbdd9d932e25ab63680bb525299e31377e09385
Webkeygr <info@webkey.gr>
2025-11-03 11:29:21 +0200

    Update Iridescence.tsx

--- app/components/Iridescence.tsx
+++ app/components/Iridescence.tsx
@@ -17,7 +17,7 @@ export default function Iridescence({
   color = [1, 1, 1],
   speed = 1.0,
   amplitude = 0.1,
-  mouseReact = true,
+  mouseReact = false,
   className = "",
 }: IridescenceProps) {
   const ctnDom = useRef<HTMLDivElement | null>(null);
