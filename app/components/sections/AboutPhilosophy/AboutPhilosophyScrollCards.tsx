b1dff7edcef7ac4aafa58d4b8dd2f75c52720150
Webkeygr <info@webkey.gr>
2025-11-28 00:52:58 +0200

    Update AboutPhilosophyScrollCards.tsx

--- app/components/sections/AboutPhilosophy/AboutPhilosophyScrollCards.tsx
+++ app/components/sections/AboutPhilosophy/AboutPhilosophyScrollCards.tsx
@@ -116,7 +116,7 @@ const AboutPhilosophyScrollCards: React.FC = () => {
   translate3d(${x}px, ${y}px, ${z}px)
   translate(-50%, -50%)
   rotateX(18deg)
-  rotateZ(-4deg)
+  rotateZ(4deg)
   scale(${scale})
 `,
 
