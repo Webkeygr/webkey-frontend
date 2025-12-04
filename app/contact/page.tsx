4ed8a74c6a994456ff8128b7cd3fb53441031600
Webkeygr <info@webkey.gr>
2025-12-05 01:18:00 +0200

    .

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,4 @@
-import ContactSection from "../../components/contact/ContactSection";
+import ContactSection from "@/app/components/contact/ContactSection";
 
 export default function ContactPage() {
   return (
