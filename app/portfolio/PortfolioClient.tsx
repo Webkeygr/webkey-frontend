7dfc640b94aa348d4c381f200f4e9e05de0768c6
Webkeygr <info@webkey.gr>
2025-12-11 23:37:37 +0200

    Update PortfolioClient.tsx

--- app/portfolio/PortfolioClient.tsx
+++ app/portfolio/PortfolioClient.tsx
@@ -72,7 +72,7 @@ export default function PortfolioClient({ projects }: PortfolioClientProps) {
     clone.style.borderRadius = "32px";
     clone.style.objectFit = "cover";
     clone.style.pointerEvents = "none";
-    clone.style.boxShadow = "0 30px 80px rgba(0,0,0,0.45)`;
+    clone.style.boxShadow = "0 30px 80px rgba(0,0,0,0.45)";
     clone.style.transformOrigin = "50% 50%";
 
     document.body.appendChild(clone);
